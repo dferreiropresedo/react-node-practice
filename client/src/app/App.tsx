2503f871edb9ff31f95c0d75fe411e70a29baba9
Daniel Ferreiro <danielferreiro.8.10.95@gmail.com>
2023-06-06 22:04:59 +0200

    Initial commit

---
+++ client/src/app/App.tsx
@@ -0,0 +1,30 @@
+import Task from "../components/task/Task";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Task />
+    </>
+  );
+}
+
+export default App;
+
+/**
+ * <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet" />
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+<div class="task">
+  <p class="task-title">Tarea sin importancia</p>
+  <i class="fa fa-trash"></i>
+  <hr />
+  <i class="fa fa-calendar"></i>
+  <p class="task-date">02/02/2023</p>
+</div>
+
+ * 
+ */
